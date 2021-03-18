d09df69f38de55959b4e86a1f935f84267e0eb8b
ricree <ricree@gmail.com>
2021-03-18 12:48:29 -0500

    Switched Auth UI updates to setValue on main thread using withContext

--- app/src/main/java/com/test/propertymanagementapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/test/propertymanagementapp/ui/auth/AuthViewModel.kt
@@ -14,43 +14,45 @@ import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
-class AuthViewModel(private val repository: AuthRepository, private val validator: AuthValidator)  : ViewModel(){
+class AuthViewModel(private val repository: AuthRepository, private val validator: AuthValidator) :
+    ViewModel() {
     val user = RegistrationUser()
-    val confirmPassword = MutableLiveData<String>().apply { value="" }
-    val accountType = MutableLiveData<AccountType>().apply { value=AccountType.tenant }
-    val errorMessage =  MutableLiveData<String>().apply { value="" }
-    val currentAction = MutableLiveData<AuthAction>().apply{value=AuthAction.PENDING}
+    val confirmPassword = MutableLiveData<String>().apply { value = "" }
+    val accountType = MutableLiveData<AccountType>().apply { value = AccountType.tenant }
+    val errorMessage = MutableLiveData<String>().apply { value = "" }
+    val currentAction = MutableLiveData<AuthAction>().apply { value = AuthAction.PENDING }
 
 
-
-    fun loginButtonClick(view: View){
+    fun loginButtonClick(view: View) {
 //        val password = password.value
 //        val email = email.value
+        errorMessage.value=""
         val email = user.email
         val password = user.password
         val user = user
-        Log.d("myapp","Checking Login")
+        Log.d("myapp", "Checking Login")
         val result = validator.validateLogin(user)
-        if(result==AuthValidationResult.Success) {
-            viewModelScope.launch(Dispatchers.Default) {
+        if (result == AuthValidationResult.Success) {
+            viewModelScope.launch(Dispatchers.IO) {
                 try {
                     handleSuccess(repository.login(email!!, password!!), AuthAction.LOGIN)
-                }catch (e:Throwable){
+                } catch (e: Throwable) {
                     handleError(e)
                 }
             }
             //should be safe to cast because validator checks
-        }else{
+        } else {
             errorMessage.value = result.message
         }
     }
 
 
-
-    fun registerUser(view:View){
-        Log.d("myapp","Begining registration")
+    fun registerUser(view: View) {
+        Log.d("myapp", "Begining registration")
+        errorMessage.value=""
         val password = user.password
         val email = user.email
         val landlordEmail = user.landlordEmail
@@ -58,91 +60,69 @@ class AuthViewModel(private val repository: AuthRepository, private val validato
         val confirmPassword = confirmPassword.value
         val name = user.name
         val result = validator.validateRegistration(
-                name = name,
-                password = password,
-                confirmPassword = confirmPassword,
-                email = email,
-                landlordEmail = landlordEmail,
-                checkTenant = type==AccountType.tenant
+            name = name,
+            password = password,
+            confirmPassword = confirmPassword,
+            email = email,
+            landlordEmail = landlordEmail,
+            checkTenant = type == AccountType.tenant
         )
-        if(result==AuthValidationResult.Success){
-            val user = RegistrationUser(name=name,
+        if (result == AuthValidationResult.Success) {
+            val user = RegistrationUser(
+                name = name,
                 password = password,
                 email = email,
                 landlordEmail = landlordEmail,
                 type = type
             )
-            viewModelScope.launch {
+            viewModelScope.launch(Dispatchers.Default) {
                 try {
                     handleSuccess(repository.register(user), AuthAction.REGISTER)
-                }catch(e:Throwable){
+                } catch (e: Throwable) {
                     handleError(e)
                 }
             }
-        }else{
+        } else {
             errorMessage.value = result.message
         }
     }
 
-    private fun handleSuccess(response: AuthResponse, successAction: AuthAction){
-        Log.d("myapp","success: $response")
-        if(response.error) {
-            Log.d("myapp","Error: ${response.message}")
-
-//                val response = Gson().fromJson()
-            errorMessage.postValue(response.message ?: "Error encountered")
-        }else{
-            currentAction.postValue(successAction)
-        }
-    }
-
-    private fun handleError(e: Throwable){
-        if(e is HttpException){
-            val msg = e.response()?.errorBody()?.string()
-            Log.d("myapp",msg?:"Error")
-            val response = Gson().fromJson(msg, AuthResponse::class.java)
-            val err = response?.message ?: e.localizedMessage?:"Error"
-            Log.e("myapp",err)
-            errorMessage.postValue(err)
-        }else{
-            Log.e("myapp","Generic error")
-            Log.e("myapp", "$e    ${e.message}    ${e.localizedMessage}")
-            errorMessage.postValue(e.localizedMessage)
-        }
-    }
-
-
-    inner class AuthObserver(private val successAction:AuthAction) : SingleObserver<AuthResponse>{
-        override fun onSubscribe(d: Disposable) {
-        }
-
-        override fun onSuccess(t: AuthResponse) {
-            Log.d("myapp","success: $t")
-            if(t.error) {
-                Log.d("myapp","Error: ${t.message}")
-
-//                val response = Gson().fromJson()
-                errorMessage.value = t.message ?: "Error encountered"
-            }else{
+    private suspend fun handleSuccess(response: AuthResponse, successAction: AuthAction) {
+        Log.d("myapp", "success: $response")
+        withContext(Dispatchers.Main) {
+            if (response.error) {
+                Log.d("myapp", "Error: ${response.message}")
+                errorMessage.value = response.message ?: "Error encountered"
+            } else {
                 currentAction.value = successAction
             }
         }
+    }
 
-        override fun onError(e: Throwable) {
-            Log.d("myapp","error: $e")
-            if(e is HttpException){
+    private suspend fun handleError(e: Throwable) {
+        withContext(Dispatchers.Main) {
+            if (e is HttpException) {
                 val msg = e.response()?.errorBody()?.string()
-                Log.d("myapp",msg?:"Error")
+                Log.d("myapp", msg ?: "Error")
                 val response = Gson().fromJson(msg, AuthResponse::class.java)
-                val err = response?.message ?: e.localizedMessage?:"Error"
-                Log.d("myapp",err)
-                errorMessage.value =err
-            }else{
+                val err = response?.message ?: e.localizedMessage ?: "Error"
+                Log.e("myapp", err)
+                errorMessage.value = err
+            } else {
+                Log.e("myapp", "Generic error")
+                Log.e("myapp", "$e    ${e.message}    ${e.localizedMessage}")
                 errorMessage.value = e.localizedMessage
             }
-        }
 
-//        override fun onNext(t: AuthResponse) {
+        }
+    }
+
+
+//    inner class AuthObserver(private val successAction:AuthAction) : SingleObserver<AuthResponse>{
+//        override fun onSubscribe(d: Disposable) {
+//        }
+//
+//        override fun onSuccess(t: AuthResponse) {
 //            Log.d("myapp","success: $t")
 //            if(t.error) {
 //                Log.d("myapp","Error: ${t.message}")
@@ -154,8 +134,34 @@ class AuthViewModel(private val repository: AuthRepository, private val validato
 //            }
 //        }
 //
-//        override fun onComplete() {
+//        override fun onError(e: Throwable) {
+//            Log.d("myapp","error: $e")
+//            if(e is HttpException){
+//                val msg = e.response()?.errorBody()?.string()
+//                Log.d("myapp",msg?:"Error")
+//                val response = Gson().fromJson(msg, AuthResponse::class.java)
+//                val err = response?.message ?: e.localizedMessage?:"Error"
+//                Log.d("myapp",err)
+//                errorMessage.value =err
+//            }else{
+//                errorMessage.value = e.localizedMessage
+//            }
 //        }
-
-    }
+//
+////        override fun onNext(t: AuthResponse) {
+////            Log.d("myapp","success: $t")
+////            if(t.error) {
+////                Log.d("myapp","Error: ${t.message}")
+////
+//////                val response = Gson().fromJson()
+////                errorMessage.value = t.message ?: "Error encountered"
+////            }else{
+////                currentAction.value = successAction
+////            }
+////        }
+////
+////        override fun onComplete() {
+////        }
+//
+//    }
 }
